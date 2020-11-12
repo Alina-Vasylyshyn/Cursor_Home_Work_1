e369b66463b45dbc25d94dc1849029216938852d
Alina <vasylyshyn.alina@gmail.com>
2020-11-12 20:17:07 +0200

    Initial commit

---
+++ src/HomeWork1.java
@@ -0,0 +1,39 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        byte a = 25; byte b = 5;
+        System.out.println("Addition: " + a + " + " + b + " = " + (a + b));
+        System.out.println("Subtraction: " + a + " - " + b + " = " + (a - b));
+        System.out.println("Multiplication: " + a + " * " + b + " = " + (a * b));
+        System.out.println("Division: " + a + " / " + b + " = " + (a / b));
+        System.out.println();
+
+        short a_1 = 250; short b_1 = 25;
+        System.out.println("Addition: " + a_1 + " + " + b_1 + " = " + (a_1 + b_1));
+        System.out.println("Subtraction: " + a_1 + " - " + b_1 + " = " + (a_1 - b_1));
+        System.out.println("Multiplication: " + a_1 + " * " + b_1 + " = " + (a_1 * b_1));
+        System.out.println("Division: " + a_1 + " / " + b_1 + " = " + (a_1 / b_1));
+        System.out.println();
+
+        long a_2 = 1080000L; long b_2 = 540000L;
+        System.out.println("Addition: " + a_2 + " + " + b_2 + " = " + (a_2 + b_2));
+        System.out.println("Subtraction: " + a_2 + " - " + b_2 + " = " + (a_2 - b_2));
+        System.out.println("Multiplication: " + a_2 + " * " + b_2 + " = " + (a_2 * b_2));
+        System.out.println("Division: " + a_2 + " / " + b_2 + " = " + (a_2 / b_2));
+        System.out.println();
+
+        double a_3 = 8.6; double b_3 = 4.3;
+        System.out.println("Addition: " + a_3 + " + " + b_3 + " = " + (a_3 + b_3));
+        System.out.println("Subtraction: " + a_3 + " - " + b_3 + " = " + (a_3 - b_3));
+        System.out.println("Multiplication: " + a_3 + " * " + b_3 + " = " + (a_3 * b_3));
+        System.out.println("Division: " + a_3 + " / " + b_3 + " = " + (a_3 / b_3));
+        System.out.println();
+
+        float a_4 = 1.2f; float b_4 = 0.3f;
+        System.out.println("Addition: " + a_4 + " + " + b_4 + " = " + (a_4 + b_4));
+        System.out.println("Subtraction: " + a_4 + " - " + b_4 + " = " + (a_4 - b_4));
+        System.out.println("Multiplication: " + a_4 + " * " + b_4 + " = " + (a_4 * b_4));
+        System.out.println("Division: " + a_4 + " / " + b_4 + " = " + (a_4 / b_4));
+        System.out.println();
+
+    }
+}
