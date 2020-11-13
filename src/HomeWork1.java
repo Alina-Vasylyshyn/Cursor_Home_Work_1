0ee374f64bc0cf6f8c6c7890ac6e8670ec25481d
Alina-Vasylyshyn <71761033+Alina-Vasylyshyn@users.noreply.github.com>
2020-11-13 18:31:45 +0200

    Update HomeWork1.java

--- src/HomeWork1.java
+++ src/HomeWork1.java
@@ -1,38 +1,58 @@
 public class HomeWork1 {
     public static void main(String[] args) {
         byte a = 25; byte b = 5;
-        System.out.println("Addition: " + a + " + " + b + " = " + (a + b));
-        System.out.println("Subtraction: " + a + " - " + b + " = " + (a - b));
-        System.out.println("Multiplication: " + a + " * " + b + " = " + (a * b));
-        System.out.println("Division: " + a + " / " + b + " = " + (a / b));
+        byte sum = a + b;
+        System.out.println("Addition: " + a + " + " + b + " = " + sum);
+        byte difference = a - b;
+        System.out.println("Subtraction: " + a + " - " + b + " = " + difference);
+        byte product = a * b;
+        System.out.println("Multiplication: " + a + " * " + b + " = " + product);
+        byte quotient = a / b;
+        System.out.println("Division: " + a + " / " + b + " = " + quotient);
         System.out.println();
 
-        short a_1 = 250; short b_1 = 25;
-        System.out.println("Addition: " + a_1 + " + " + b_1 + " = " + (a_1 + b_1));
-        System.out.println("Subtraction: " + a_1 + " - " + b_1 + " = " + (a_1 - b_1));
-        System.out.println("Multiplication: " + a_1 + " * " + b_1 + " = " + (a_1 * b_1));
-        System.out.println("Division: " + a_1 + " / " + b_1 + " = " + (a_1 / b_1));
+        short a1 = 250; short b1 = 25;
+        short sum1 = a1 + b1;
+        System.out.println("Addition: " + a1 + " + " + b1 + " = " + sum1);
+        short difference1 = a1 - b1;
+        System.out.println("Subtraction: " + a1 + " - " + b1 + " = " + difference1);
+        short product1 = a1 * b1;
+        System.out.println("Multiplication: " + a1 + " * " + b1 + " = " + product1);
+        short quotient1 = a1 / b1;
+        System.out.println("Division: " + a1 + " / " + b1 + " = " + quotient1);
         System.out.println();
 
-        long a_2 = 1080000L; long b_2 = 540000L;
-        System.out.println("Addition: " + a_2 + " + " + b_2 + " = " + (a_2 + b_2));
-        System.out.println("Subtraction: " + a_2 + " - " + b_2 + " = " + (a_2 - b_2));
-        System.out.println("Multiplication: " + a_2 + " * " + b_2 + " = " + (a_2 * b_2));
-        System.out.println("Division: " + a_2 + " / " + b_2 + " = " + (a_2 / b_2));
+        long a2 = 1080000L; long b2 = 540000L;
+        long sum2 = a2 + b2;
+        System.out.println("Addition: " + a2 + " + " + b2 + " = " + sum2);
+        long difference2 = a2 - b2;
+        System.out.println("Subtraction: " + a2 + " - " + b2 + " = " + difference2);
+        long product2 = a2 * b2;
+        System.out.println("Multiplication: " + a2 + " * " + b2 + " = " + product2);
+        long quotient2 = a2 / b2;
+        System.out.println("Division: " + a2 + " / " + b2 + " = " + quotient2);
         System.out.println();
 
-        double a_3 = 8.6; double b_3 = 4.3;
-        System.out.println("Addition: " + a_3 + " + " + b_3 + " = " + (a_3 + b_3));
-        System.out.println("Subtraction: " + a_3 + " - " + b_3 + " = " + (a_3 - b_3));
-        System.out.println("Multiplication: " + a_3 + " * " + b_3 + " = " + (a_3 * b_3));
-        System.out.println("Division: " + a_3 + " / " + b_3 + " = " + (a_3 / b_3));
+        double a3 = 8.6; double b3 = 4.3;
+        double sum3 = a3 + b3;
+        System.out.println("Addition: " + a3 + " + " + b3 + " = " + sum3);
+        double difference3 = a3 - b3;
+        System.out.println("Subtraction: " + a3 + " - " + b3 + " = " + difference3);
+        double product3 = a3 * b3;
+        System.out.println("Multiplication: " + a3 + " * " + b3 + " = " + product3);
+        double quotient3 = a3 / b3;
+        System.out.println("Division: " + a3 + " / " + b3 + " = " + quotient3);
         System.out.println();
 
-        float a_4 = 1.2f; float b_4 = 0.3f;
-        System.out.println("Addition: " + a_4 + " + " + b_4 + " = " + (a_4 + b_4));
-        System.out.println("Subtraction: " + a_4 + " - " + b_4 + " = " + (a_4 - b_4));
-        System.out.println("Multiplication: " + a_4 + " * " + b_4 + " = " + (a_4 * b_4));
-        System.out.println("Division: " + a_4 + " / " + b_4 + " = " + (a_4 / b_4));
+        float a4 = 1.2f; float b4 = 0.3f;
+        float sum4 = a4 + b4;
+        System.out.println("Addition: " + a4 + " + " + b4 + " = " + sum4);
+        float difference4 = a4 - b4;
+        System.out.println("Subtraction: " + a4 + " - " + b4 + " = " + difference4);
+        float product4 = a4 * b4;
+        System.out.println("Multiplication: " + a4 + " * " + b4 + " = " + product4);
+        float quotient3 = a4 / b4;
+        System.out.println("Division: " + a4 + " / " + b4 + " = " + quotient4);
         System.out.println();
 
     }
